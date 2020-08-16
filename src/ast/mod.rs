fc538c39cd627ac6338dfcdec7f783e77bb5331f
Roman Isaikin <romix.lab@gmail.com>
2020-08-16 19:44:03 +0200

    Switch to syn, update ast nodes for Register.

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,55 +1,109 @@
-use crate::types::{Lit, RangeLit, StrLit, Ty};
+use syn::{LitStr, LitInt, ExprStruct, Expr, ExprCall, ExprClosure, Path};
 
-#[derive(Debug)]
-pub enum Value {
-    Constant(Ty),
-    Variable(Ty),
-    Resource,
-    Expression(Box<Expression>),
-}
-
-#[derive(Debug)]
-pub enum Operation {
-    Addition(Value, Value),
-    Subtraction(Value, Value),
-    Multiplication(Value, Value),
-    Division(Value, Value),
-}
-
-#[derive(Debug)]
-pub enum Expression {
-    Value(Value),
-    Operation(Operation),
-}
+// #[derive(Debug)]
+// pub enum Value {
+//     Constant(Ty),
+//     Variable(Ty),
+//     Resource,
+//     Expression(Box<Expression>),
+// }
 
 #[derive(Debug)]
 pub struct Range {
-    pub start: RangeLit,
-    pub end: RangeLit,
+    pub start: LitInt,
+    pub end: LitInt,
 }
 
 #[derive(Debug)]
 pub enum ResourceName {
-    Terminal(StrLit),
+    Plain(LitStr),
     //ArrayProduct(String, Array, String),
-    RangeProduct(StrLit, Range, StrLit),
+    RangeProduct(LitStr, Range, LitStr),
+}
+
+/// Represents bit field inside a Register or _.
+/// Endianness is not applicable here, since bit field can't be used on it's own, see Register.
+#[derive(Debug)]
+pub struct BitField {
+    /// Base type, one of: u8, u16, u32, u64, u128.
+    pub base_ty: Path,
+    /// Is writing to this bit field allowed
+    pub access_mode: AccessMode,
+    /// Starting from bit, inclusive.
+    /// Must be >= that `to`.
+    /// If from == to, only one bit is represented (flag).
+    pub from: u32,
+    /// Ending at bit, inclusive.
+    pub to: u32,
+    /// Example values for enum fields code generation, documentation and UI.
+    /// The only values that are allowed, if `is_valid` == `Some(OnlyListedValues)`.
+    pub values: Vec<ExprStruct>,
+    /// Validity check mode
+    pub is_valid: Option<ValidityCheck>
+}
+
+#[derive(Debug)]
+pub enum RegisterAddress {
+    Plain(u64),
+    Custom(ExprStruct)
+}
+
+#[derive(Debug)]
+pub enum Endianness {
+    Little,
+    Big
+}
+
+#[derive(Debug)]
+pub enum AccessMode {
+    ReadOnly,
+    WriteOnly,
+    ReadWrite
+}
+
+#[derive(Debug)]
+pub enum ValidityCheck {
+    OnlyListedValues,
+    Call(ExprCall),
+    Closure(ExprClosure)
+}
+
+/// Represents hardware register accessed by it's `address`.
+/// `address` can be a simple number (periphery register), or it can be a structure with arbitrary
+/// fields (SPI accessed register).
+/// Code generators for a particular interface decide what to do with `address`.
+#[derive(Debug)]
+pub struct Register {
+    pub address: RegisterAddress,
+    pub endianness: Endianness,
+    pub default: Option<Expr>,
+    pub description: Option<LitStr>,
+    pub values: Vec<ExprStruct>,
+    pub is_valid: Option<ValidityCheck>,
+    pub bits: Vec<BitField>
+}
+
+#[derive(Debug)]
+pub enum Type {
+    /// Hardware register
+    Register(Register),
+
 }
 
 #[derive(Debug)]
 pub enum ResourceKind {
     // set/get/subscribe(sugar on stream?), default, allowed, values
     // underlying type is required, maybe not known right away, but can be derived from bits
-    Property(Option<Ty>),
-    Function, // fn(args) -> value
-    Stream(Ty),   // value,value,value... subscribe, unsubscribe, backpressure, bandwith limit
-              //User      // everything else
+    Property(Type),
+    //Function, // fn(args) -> value
+    //Stream(Type), // value,value,value... subscribe, unsubscribe, backpressure, bandwith limit
+    //User      // everything else
 }
 
 #[derive(Debug)]
 pub struct Resource {
-    pub id: Option<u32>, // required, maybe not provided at all, it that case it can be auto assigned
-    pub name: ResourceName, // required and available right away
-    pub children: Vec<Resource>, // optional, can be 0 or more children
-    pub kind: Option<ResourceKind>, // required, but maybe not known right away
-                  //meta: // all additional fields goes here, hash Ident->Expression?
+    pub id: Option<u32>, // required, if not provided, it can be auto assigned through vhl-cli
+    pub name: ResourceName,
+    pub children: Vec<Resource>,
+    pub kind: Option<ResourceKind>,
 }
---
+++ vhl-cli/src/main.rs
@@ -0,0 +1,6 @@
+
+fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    println!("vhl");
+
+    Ok(())
+}

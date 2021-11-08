9a05ae991db569532ea4503ad5a6f6ca240214e0
Roman Isaikin <romix.lab@gmail.com>
2021-11-08 21:18:47 +0100

    Create parser lib, update Cargo.toml.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,10 +1,3 @@
-use vhl::loader::load;
-
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     println!("vhl 0.3.0");
-
-    let r = load();
-    println!("load:{:#?}", r);
-
-    Ok(())
 }

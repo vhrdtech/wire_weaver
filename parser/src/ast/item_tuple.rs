7a401aaa2c6f29703e2e04da032039cb58667151
Roman Isaikin <romix.lab@gmail.com>
2021-12-15 20:55:28 +0100

    Implement TupleFields parser.

---
+++ parser/src/ast/item_tuple.rs
@@ -0,0 +1,30 @@
+use super::prelude::*;
+use super::ty::Ty;
+
+#[derive(Debug)]
+pub struct TupleFields {
+    fields: Vec<Ty>
+}
+
+impl<'i> Parse<'i> for TupleFields {
+    fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ()> {
+        if let Some(tf) = input.pairs.peek() {
+            if tf.as_rule() == Rule::tuple_fields {
+                let tf = input.pairs.next().unwrap();
+                let mut tf = ParseInput::fork(tf, input);
+                let mut fields = Vec::new();
+                while let Some(_) = tf.pairs.peek() {
+                    tf.parse().map(|ty| fields.push(ty))?;
+                }
+
+                Ok(TupleFields {
+                    fields
+                })
+            } else {
+                Err(())
+            }
+        } else {
+            Err(())
+        }
+    }
+}

0a80a832aa92add98e996150c06167cca5844b7d
Roman Isaikin <romix.lab@gmail.com>
2022-01-16 23:38:53 +0100

    parser: extract Highlighter into its own crate: `parser-test`.

--- parser/src/lexer.rs
+++ parser/src/lexer.rs
@@ -10,8 +10,6 @@ pub struct Lexer;
 
 #[cfg(test)]
 mod test {
-    use std::str::{CharIndices};
-    use std::iter::Peekable;
     use super::{Rule, Lexer};
     use pest::Parser;
 
@@ -27,97 +25,6 @@ mod test {
     // fn test(input: &str, div11: &str) {
     //
     // }
-
-    /// Helper type that create (start, end) iterator over strings like "^ ^--^ ^^".
-    /// Allows to write parser tests as follows:
-    /// test(
-    ///      "struct X { field: u32 }",
-    ///      "       ^   ^---^  ^-^  ",
-    ///      type_name("My"),
-    ///      ident_name("field"),
-    ///      any_ty("u32")
-    /// );
-    struct Highlighter<'a> {
-        spans: Peekable<CharIndices<'a>>,
-    }
-    impl<'a> Highlighter<'a> {
-        fn new(spans: &'a str) -> Highlighter<'a> {
-            Self {
-                spans: spans.char_indices().peekable(),
-            }
-        }
-    }
-    impl<'a> Iterator for Highlighter<'a> {
-        type Item = (usize, usize);
-
-        fn next(&mut self) -> Option<Self::Item> {
-            match self.spans.next() {
-                Some((start, c)) => {
-                    assert_eq!(c, '^');
-
-                    let mut end = start;
-                    loop {
-                        match self.spans.next() {
-                            Some((pos, c)) => {
-                                if c.is_whitespace() {
-                                    break;
-                                } else if c == '-' {
-                                    match self.spans.peek() {
-                                        Some((_, c)) => {
-                                            if c.is_whitespace() {
-                                                panic!("Wrong highlighter string: ^--^ sequence unterminated");
-                                            }
-                                        }
-                                        None => {
-                                            panic!("Wrong highlighter string: ^--^ sequence unterminated");
-                                        }
-                                    }
-                                    continue;
-                                } else if c == '^' {
-                                    end = pos;
-                                } else {
-                                    panic!("Wrong highlighter string: only '^', '-' and ' ' are allowed");
-                                }
-                            }
-                            None => {
-                                break;
-                            }
-                        }
-                    }
-                    Some((start, end))
-                }
-                None => None
-            }
-        }
-    }
-
-    #[test]
-    fn test_highlighter() {
-        let mut hl = Highlighter::new("^");
-        assert_eq!(hl.next(), Some((0, 0)));
-        assert_eq!(hl.next(), None);
-
-        let mut hl = Highlighter::new("^ ^");
-        assert_eq!(hl.next(), Some((0, 0)));
-        assert_eq!(hl.next(), Some((2, 2)));
-        assert_eq!(hl.next(), None);
-
-        let mut hl = Highlighter::new("^-^ ^^ ^--^ ^ ^----^");
-        assert_eq!(hl.next(), Some((0, 2)));
-        assert_eq!(hl.next(), Some((4, 5)));
-        assert_eq!(hl.next(), Some((7, 10)));
-        assert_eq!(hl.next(), Some((12, 12)));
-        assert_eq!(hl.next(), Some((14, 19)));
-        assert_eq!(hl.next(), None);
-
-        let mut hl = Highlighter::new("^--");
-        let r = std::panic::catch_unwind(move || hl.next());
-        assert!(r.is_err());
-
-        let mut hl = Highlighter::new("^-- ");
-        let r = std::panic::catch_unwind(move || hl.next());
-        assert!(r.is_err());
-    }
 
     #[test]
     fn test_discrete_numbers() {

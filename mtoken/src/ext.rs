b64c89b0e5a80ceccd15f5734a0de6a750fc8cdf
Roman Isaikin <romix.lab@gmail.com>
2022-01-16 23:39:54 +0100

    mtoken: append, append_all and append_separated into TokenStreamExt trait.

---
+++ mtoken/src/ext.rs
@@ -0,0 +1,57 @@
+use crate::{TokenTree, ToTokens, TokenStream};
+use std::iter;
+
+pub trait TokenStreamExt: private::Sealed {
+    fn append<U>(&mut self, token: U)
+        where
+            U: Into<TokenTree>;
+
+    fn append_all<I>(&mut self, iter: I)
+        where
+            I: IntoIterator,
+            I::Item: ToTokens;
+
+    fn append_separated<I, U>(&mut self, iter: I, op: U)
+        where
+            I: IntoIterator,
+            I::Item: ToTokens,
+            U: ToTokens;
+}
+
+impl TokenStreamExt for TokenStream {
+    fn append<U>(&mut self, token: U)
+        where U: Into<TokenTree>
+    {
+        self.extend(iter::once(token.into()));
+    }
+
+    fn append_all<I>(&mut self, iter: I)
+        where I: IntoIterator,
+              I::Item: ToTokens
+    {
+        for token in iter {
+            token.to_tokens(self);
+        }
+    }
+
+    fn append_separated<I, U>(&mut self, iter: I, op: U)
+        where
+            I: IntoIterator,
+            I::Item: ToTokens,
+            U: ToTokens,
+    {
+        for (i, token) in iter.into_iter().enumerate() {
+            if i > 0 {
+                op.to_tokens(self);
+            }
+            token.to_tokens(self);
+        }
+    }
+}
+
+mod private {
+    use crate::TokenStream;
+
+    pub trait Sealed {}
+    impl Sealed for TokenStream {}
+}

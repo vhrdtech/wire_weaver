ddac87113bf7f896365891a0dea4b36f7f57b581
Roman Isaikin <romix.lab@gmail.com>
2022-07-27 10:28:31 +0200

    cli: Add temporary test code.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,24 +1,13 @@
 use parser::ast::file::File;
-use parser::ast::item::Item;
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
-    let input = r#"
-        /// Line 1
-        /// Line 2
-        enum FrameId {
-            Standard(u11),
-            Extended(u29)
-        }"#;
-    let file = File::parse(input)?;
-    // println!("Warnings: {:?}", file.1);
-    println!("{:?}", file.0.items[0]);
+    let input = std::fs::read_to_string("/Users/roman/git/vhl_hw/led_ctrl/led_ctrl.vhl")?;
+    // let s = parser::util::pest_file_parse_tree(&input);
+    // println!("{}", s);
 
-    match &file.0.items[0] {
-        Item::Const(_) => {}
-        Item::Enum(ie) => {
-            println!("{:?}", codegen::fun(ie));
-        }
-    }
+    let file = File::parse(&input)?;
+    println!("Warnings: {:?}", file.1);
+    println!("File: {:#?}", file.0);
 
     // codegen::fun2();
 

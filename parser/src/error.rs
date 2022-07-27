54c846bb6d57c41d696808a4dd44cdaf43063af4
Roman Isaikin <romix.lab@gmail.com>
2022-07-27 13:22:52 +0200

    parser: Improve error handling and add documentation to various parts of the parser. Implement fork_at() and parse_or_skip() to simplify parsers.

--- parser/src/error.rs
+++ parser/src/error.rs
@@ -16,12 +16,23 @@ pub enum ParseErrorKind {
 
 #[derive(PartialEq, Eq)]
 pub enum ParseErrorSource {
-    Internal,
-    User
+    /// Parser internal error, for example if a feature is not implemented.
+    /// unreachable() and unwrap()'s are converted into this error as well.
+    /// Will be pushed onto error list in `ast/file.rs`, so that no errors are silently ignored.
+    /// More precise errors might be pushed onto the same list by parsers.
+    InternalError,
+    /// Not enough input or unexpected rule (because expected one is absent).
+    /// Might not be an error like in enum with only discriminant values.
+    /// The only error to be ignored by `parse_or_skip()`, so that parsing of the
+    /// current node can continue.
+    /// Will be pushed onto error list in `ast/file.rs` if not ignored along the way.
+    UnexpectedInput,
+    /// User provided erroneous input, invalid number for example.
+    UserError
 }
 
 impl ParseErrorSource {
     pub fn is_internal(&self) -> bool {
-        *self == ParseErrorSource::Internal
+        *self == ParseErrorSource::InternalError
     }
 }
--- parser/src/parse.rs
+++ parser/src/parse.rs
@@ -29,6 +29,25 @@ impl<'i, 'm> ParseInput<'i, 'm> {
         }
     }
 
+    /// Given the following pairs tree:
+    /// rule_a, rule_b, rule_c, ...
+    ///   |_ rule_a1, rule_a2, ...
+    /// Where rula_ai is children rules inside rula_a.
+    ///
+    /// If called with expected_rule == rule_a, will return Ok with input on rula_a1 ready to be parsed.
+    /// Otherwise UnexpectedInput error will be return, that can be ignored by `parse_or_skip()`
+    /// or propagated and recorded.
+    pub fn fork_at(mut self, expected_rule: Rule) -> Result<ParseInput<'i, 'm>, ParseErrorSource> {
+        let pair = self.pairs.next().ok_or(ParseErrorSource::UnexpectedInput)?;
+        if pair.as_rule() == expected_rule {
+            Ok(ParseInput {
+                pairs: pair.into_inner(), warnings: self.warnings, errors: self.errors
+            })
+        } else {
+            Err(ParseErrorSource::UnexpectedInput)
+        }
+    }
+
     pub fn next1(&mut self, rule1: Rule) -> Option<Pair<'i, Rule>> {
         match self.pairs.next() {
             Some(p1) => {
@@ -80,11 +99,34 @@ impl<'i, 'm> ParseInput<'i, 'm> {
 
 /// Parsing interface implemented by all AST nodes
 pub trait Parse<'i>: Sized {
+    /// Try to parse input, return Ok(Some(Self)) if succeeded, Ok(None) if input is absent and it
+    /// is expected, Err if user or internal error was encountered.
+    /// While parsing, push warnings and errors into respective lists.
     fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource>;
 }
 
 impl<'i, 'm> ParseInput<'i, 'm> {
+    /// Try to parse input stream into an Ok(AST node). Return error otherwise.
     pub fn parse<T: Parse<'i>>(&mut self) -> Result<T, ParseErrorSource> {
         T::parse(self)
+    }
+
+    /// Try to parse input stream into an Ok(Option<AST node>). If failed with UnexpectedInput,
+    /// ignore and return Ok(None).
+    /// Parsing can continue, leaving None in a field, where that was intended.
+    ///
+    /// Otherwise return original error that will be propagated further and parsing of the current
+    /// node will fail.
+    pub fn parse_or_skip<T: Parse<'i>>(&mut self) -> Result<Option<T>, ParseErrorSource> {
+        match T::parse(self) {
+            Ok(t) => Ok(Some(t)),
+            Err(e) => {
+                match e {
+                    e @ ParseErrorSource::InternalError => Err(e),
+                    ParseErrorSource::UnexpectedInput => Ok(None),
+                    e @ ParseErrorSource::UserError => Err(e)
+                }
+            }
+        }
     }
 }

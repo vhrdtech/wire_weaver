3678baf4fe9b8caaf61804f5a56936180b59f46d
Roman Isaikin <romix.lab@gmail.com>
2022-07-31 15:21:12 +0200

    parser: BinaryOp: Update parser.

--- parser/src/ast/item_op.rs
+++ parser/src/ast/item_op.rs
@@ -1,10 +1,10 @@
 use super::prelude::*;
 
 #[derive(Debug, Eq, PartialEq)]
-pub enum ItemOp {
+pub enum BinaryOp {
+    Negate,
     Minus,
     Plus,
-    Negate,
     Mul,
     Div,
     Rem,
@@ -17,51 +17,42 @@ pub enum ItemOp {
     Rsh,
     ClosedRange,
     OpenRange,
-    Dot
+    Dot,
+    LParen,
+    RParen,
 }
 
-impl ItemOp {
-    pub fn from_str(s: &str) -> Self {
-        use ItemOp::*;
-        match s {
-            "-" => Minus,
-            "+" => Plus,
-            "!" => Negate,
-            "*" => Mul,
-            "/" => Div,
-            "%" => Rem,
-            "&&" => BoolAnd,
-            "||" => BoolOr,
-            "&" => BitAnd,
-            "|" => BitOr,
-            "^" => Xor,
-            "<<" => Lsh,
-            ">>" => Rsh,
-            "..=" => ClosedRange,
-            ".." => OpenRange,
-            "." => Dot,
-            _ => unreachable!()
+impl BinaryOp {
+    pub fn is_range_op(&self) -> bool {
+        *self == BinaryOp::ClosedRange || *self == BinaryOp::OpenRange
+    }
+
+    pub fn from_rule(rule: Rule) -> Result<Self, ParseErrorSource> {
+        match rule {
+            Rule::op_not => Ok(BinaryOp::Negate),
+            Rule::op_plus => Ok(BinaryOp::Plus),
+            Rule::op_minus => Ok(BinaryOp::Minus),
+            Rule::op_mul => Ok(BinaryOp::Mul),
+            Rule::op_div => Ok(BinaryOp::Div),
+            Rule::op_rem => Ok(BinaryOp::Rem),
+            Rule::op_bool_and => Ok(BinaryOp::BoolAnd),
+            Rule::op_bit_and => Ok(BinaryOp::BitAnd),
+            Rule::op_bool_or => Ok(BinaryOp::BoolOr),
+            Rule::op_bit_or => Ok(BinaryOp::BitOr),
+            Rule::op_xor => Ok(BinaryOp::Xor),
+            Rule::op_lsh => Ok(BinaryOp::Lsh),
+            Rule::op_rsh => Ok(BinaryOp::Rsh),
+            Rule::op_closed_range => Ok(BinaryOp::ClosedRange),
+            Rule::op_open_range => Ok(BinaryOp::OpenRange),
+            Rule::op_dot => Ok(BinaryOp::Dot),
+            _ => Err(ParseErrorSource::internal())
         }
     }
-
-    pub fn is_range(&self) -> bool {
-        *self == ItemOp::ClosedRange || *self == ItemOp::OpenRange
-    }
 }
 
-impl<'i> Parse<'i> for ItemOp {
+impl<'i> Parse<'i> for BinaryOp {
     fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource> {
-        match input.pairs.next() {
-            Some(op) => {
-                if op.as_rule() == Rule::unary_op || op.as_rule() == Rule::binary_op {
-                    Ok(ItemOp::from_str(op.as_str()))
-                } else {
-                    Err(ParseErrorSource::internal())
-                }
-            },
-            None => {
-                Err(ParseErrorSource::internal())
-            }
-        }
+        let op = input.expect1(Rule::op_binary)?;
+        Ok(BinaryOp::from_rule(op.as_rule())?)
     }
 }

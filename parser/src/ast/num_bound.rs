f283ebf02255aacab6a464d4b8c271183fd76ade
Roman Isaikin <romix.lab@gmail.com>
2022-08-07 14:09:12 +0200

    parser: Numerical bounds: implement parser for unbound (?), max N and set of ranges.

--- parser/src/ast/num_bound.rs
+++ parser/src/ast/num_bound.rs
@@ -1,15 +1,42 @@
+use crate::ast::expr::Expr;
+use crate::error::{ParseError, ParseErrorKind};
 use super::prelude::*;
 
 #[derive(Debug)]
-pub struct NumBound<'i> {
-    pub x: &'i str,
+pub enum NumBound<'i> {
+    Unbound,
+    MaxBound(u64),
+    Set(Vec<Expr<'i>>)
 }
 
 impl<'i> Parse<'i> for NumBound<'i> {
-    fn parse<'m>(_input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource> {
-        Ok(NumBound {
-            x: "dummy"
-        })
+    fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource> {
+        let mut input = ParseInput::fork(input.expect1(Rule::num_bound)?, input);
+        let bound = input.pairs.next().ok_or_else(|| ParseErrorSource::internal("wrong num_bound rule"))?;
+        match bound.as_rule() {
+            Rule::num_unbound => Ok(NumBound::Unbound),
+            Rule::num_bound_max => {
+                let dec_lit_raw = bound.into_inner().next().ok_or_else(||
+                    ParseErrorSource::internal("wrong num_bound_list rule")
+                )?;
+                let max: u64 = dec_lit_raw.as_str().parse().map_err(|_| {
+                    input.errors.push(ParseError {
+                        kind: ParseErrorKind::IntParseError,
+                        rule: Rule::dec_lit,
+                        span: (dec_lit_raw.as_span().start(), dec_lit_raw.as_span().end())
+                    });
+                    ParseErrorSource::UserError
+                })?;
+                Ok(NumBound::MaxBound(max))
+            }
+            Rule::num_bound_list => {
+                let mut input = ParseInput::fork(bound, &mut input);
+                Ok(NumBound::Set(input.parse()?))
+            }
+            _ => {
+                return Err(ParseErrorSource::internal("wrong num_bound rule"))
+            }
+        }
     }
 }
 

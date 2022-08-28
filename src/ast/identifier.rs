c4bc0bd3e37b20721b000af57196bab98edec002
Roman Isaikin <romix.lab@gmail.com>
2022-08-28 13:51:09 +0200

    core: Update Identifier converter macro: create Identifier with context enum from parser's generic Identifier<K>.

--- src/ast/identifier.rs
+++ src/ast/identifier.rs
@@ -20,7 +20,7 @@ pub enum IdentifierContext {
     PathSegment,
 
     /// /**resource**, /**ch**`1..3`
-    XpiUriSegment,
+    XpiUriSegmentName,
 
     /// /x { **key_name**: value; }
     XpiKeyName,
@@ -48,31 +48,36 @@ pub enum IdentifierContext {
 
     /// enum MyEnum { **Field1**, **Field2** }
     EnumFieldName,
+
+    /// fn fun<**GN**>() {}
+    GenericName,
 }
 
 macro_rules! impl_from_parser_struct {
-    ($from: ident, $context: ident) => {
-        impl<'i> From<parser::ast::naming::$from<'i>> for Identifier {
-            fn from(t: parser::ast::naming::$from<'i>) -> Self {
+    ($kind: ident) => {
+        impl<'i> From<parser::ast::naming::Identifier<'i, $kind>> for Identifier {
+            fn from(t: parser::ast::naming::Identifier<'i, $kind>) -> Self {
                 Identifier {
                     symbols: Rc::new(t.name.to_string()),
-                    context: IdentifierContext::$context,
+                    context: IdentifierContext::$kind,
                     span: t.span.into()
                 }
             }
         }
     }
 }
-
-impl_from_parser_struct!(Typename, UserTyName);
-impl_from_parser_struct!(BuiltinTypename, BuiltinTyName);
-impl_from_parser_struct!(PathSegment, PathSegment);
-impl_from_parser_struct!(EnumEntryName, EnumFieldName);
-impl_from_parser_struct!(XpiUriNamedPart, XpiUriSegment);
-impl_from_parser_struct!(XpiKeyName, XpiKeyName);
-impl_from_parser_struct!(FnName, FnName);
-impl_from_parser_struct!(FnArgName, FnArgName);
-impl_from_parser_struct!(LetStmtName, VariableDefName);
-impl_from_parser_struct!(Identifier, VariableRefName);
-impl_from_parser_struct!(StructFieldName, StructFieldName);
-
+use parser::ast::naming::*;
+impl_from_parser_struct!(UserTyName);
+impl_from_parser_struct!(BuiltinTyName);
+impl_from_parser_struct!(PathSegment);
+impl_from_parser_struct!(XpiUriSegmentName);
+impl_from_parser_struct!(XpiKeyName);
+impl_from_parser_struct!(FnName);
+impl_from_parser_struct!(FnArgName);
+impl_from_parser_struct!(VariableDefName);
+impl_from_parser_struct!(VariableRefName);
+impl_from_parser_struct!(StructTyName);
+impl_from_parser_struct!(StructFieldName);
+impl_from_parser_struct!(EnumTyName);
+impl_from_parser_struct!(EnumFieldName);
+impl_from_parser_struct!(GenericName);

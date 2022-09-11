d9a94ab3af43e206d51449ec817d06abb73177fd
Roman Isaikin <romix.lab@gmail.com>
2022-09-11 22:01:16 +0200

    parser + grammar: Ignore < and > in delimiters check for now, as it results in false positives on -> in function return positions.

--- parser/src/user_readable.rs
+++ parser/src/user_readable.rs
@@ -5,6 +5,9 @@ pub fn rule_names(rule: &Rule) -> String {
         Rule::file => "definition or inner_attribute".to_owned(),
         Rule::xpi_block => "/xpi_block".to_owned(),
         Rule::xpi_impl => "use XpiTrait;".to_owned(),
+        Rule::any_ty => "type name".to_owned(),
+        Rule::resource_cell_ty => "Cell< (wo|rw)? (+stream|+observe)? type_name >".to_owned(),
+        Rule::access_mode => "const|ro|rw|wo".to_owned(),
         _ => format!("{:?}", rule),
     }
 }

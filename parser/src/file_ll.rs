f6f76b43d4b19105dbf1a4f5831d8027fedbd63c
Roman Isaikin <romix.lab@gmail.com>
2022-09-11 12:34:02 +0200

    parser: Render unmatched delimiters error with all the bells and whistles.

--- parser/src/file_ll.rs
+++ parser/src/file_ll.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Display, Formatter};
 use pest::{Position, Span};
 use crate::lexer::{Lexer, Rule};
 
@@ -16,17 +17,21 @@ pub struct LLFile<'i> {
 pub enum DelimError {
     UnexpectedClosing {
         delim: char,
-        line_col: (usize, usize)
+        line_col: (usize, usize),
+        line: String,
     },
     Unmatched {
         open_delim: char,
         open_line_col: (usize, usize),
+        open_line: String,
         close_delim: char,
         close_line_col: (usize, usize),
+        close_line: String,
     },
     ClosingNotFound {
         open_delim: char,
         open_line_col: (usize, usize),
+        open_line: String,
     }
 }
 
@@ -80,13 +85,19 @@ impl<'i> LLFile<'i> {
                                 return Err(DelimError::Unmatched {
                                     open_delim: **c_open,
                                     open_line_col: open_pos.line_col(),
+                                    open_line: open_pos.line_of().to_owned(),
                                     close_delim: *c,
-                                    close_line_col: pos.line_col()
+                                    close_line_col: pos.line_col(),
+                                    close_line: pos.line_of().to_owned()
                                 })
                             }
                         },
                         None => {
-                            return Err(DelimError::UnexpectedClosing { delim: *c, line_col: pos.line_col() })
+                            return Err(DelimError::UnexpectedClosing {
+                                delim: *c,
+                                line_col: pos.line_col(),
+                                line: pos.line_of().to_owned()
+                            })
                         }
                     }
                 }
@@ -97,7 +108,8 @@ impl<'i> LLFile<'i> {
             Some((delim, pos)) => {
                Err(DelimError::ClosingNotFound {
                    open_delim: **delim,
-                   open_line_col: pos.line_col()
+                   open_line_col: pos.line_col(),
+                   open_line: pos.line_of().to_owned(),
                })
             },
             None => Ok(())
@@ -112,5 +124,121 @@ fn is_matching_delim(first: char, second: char) -> bool {
         '[' => second == ']',
         '<' => second == '>',
         _ => unreachable!()
+    }
+}
+
+impl DelimError {
+    pub fn format(&self) -> String {
+        let spacing = self.spacing();
+        match self {
+            DelimError::UnexpectedClosing { delim, line_col, line } => {
+                format!(
+                    "\x1b[31m\x1b[1merror\x1b[39m: unmatched delimiters\x1b[0m\n\
+                     {s    }{b}-->{r} {p}:{l}:{c}\n\
+                     {s    }{b} |{r}\n\
+                     {b}{l:w$ } |{r} {line}\
+                     {s    }{b} |{r} {underline}\n\
+                     {s    }{b} |{r}\n\
+                     {s    }{b} = \x1b[39mnote:{r} Unexpected closing delimiter '{cd}'",
+                    s = spacing,
+                    p = "path.vhl",
+                    l = line_col.0,
+                    c = line_col.1,
+                    w = spacing.len(),
+                    line = line,
+                    underline = Self::underline(line.as_str(), line_col.1),
+                    cd = delim,
+                    b = "\x1b[34m\x1b[1m",
+                    r = "\x1b[0m"
+                )
+            }
+            DelimError::Unmatched { open_delim, open_line_col, open_line, close_delim, close_line_col, close_line } => {
+                format!(
+                    "\x1b[31m\x1b[1merror\x1b[39m: unmatched delimiters\x1b[0m\n\
+                     {s    }{b}-->{r} {p}:{ol}:{oc}\n\
+                     {s    }{b} |{r}\n\
+                     {b}{ol:w$} |{r} {oline}\
+                     {s    }{b} |{r} {o_underline}\n\
+                     {s    }{b} |{r} ...\n\
+                     {b}{cl:w$} |{r} {cline}\
+                     {s    }{b} |{r} {c_underline}\n\
+                     {s    }{b} |{r}\n\
+                     {s    }{b} = \x1b[39mnote:{r} Open delimiter '{od}' is closed with incorrect '{cd}'",
+                    s = spacing,
+                    p = "path.vhl",
+                    ol = open_line_col.0,
+                    oc = open_line_col.1,
+                    cl = close_line_col.0,
+                    w = spacing.len(),
+                    oline = open_line,
+                    cline = close_line,
+                    o_underline = Self::underline(open_line.as_str(), open_line_col.1),
+                    c_underline = Self::underline(close_line.as_str(), close_line_col.1),
+                    od = open_delim,
+                    cd = close_delim,
+                    b = "\x1b[34m\x1b[1m",
+                    r = "\x1b[0m"
+                )
+            }
+            DelimError::ClosingNotFound { open_delim, open_line_col, open_line } => {
+                format!(
+                    "\x1b[31m\x1b[1merror\x1b[39m: unmatched delimiters\x1b[0m\n\
+                     {s    }{b}-->{r} {p}:{l}:{c}\n\
+                     {s    }{b} |{r}\n\
+                     {b}{l:w$ } |{r} {line}\
+                     {s    }{b} |{r} {underline}\n\
+                     {s    }{b} |{r}\n\
+                     {s    }{b} = \x1b[39mnote:{r} Found open delimiter: '{od}', but no closing one for it",
+                    s = spacing,
+                    p = "path.vhl",
+                    l = open_line_col.0,
+                    c = open_line_col.1,
+                    w = spacing.len(),
+                    line = open_line,
+                    underline = Self::underline(open_line.as_str(), open_line_col.1),
+                    od = open_delim,
+                    b = "\x1b[34m\x1b[1m",
+                    r = "\x1b[0m"
+                )
+            }
+        }
+    }
+
+    fn spacing(&self) -> String {
+        let line = match self {
+            DelimError::UnexpectedClosing { line_col, .. } => line_col.0,
+            DelimError::Unmatched { close_line_col, .. } => close_line_col.0,
+            DelimError::ClosingNotFound { open_line_col, .. } => open_line_col.0,
+        };
+
+        let line_str_len = format!("{}", line).len();
+
+        let mut spacing = String::new();
+        for _ in 0..line_str_len {
+            spacing.push(' ');
+        }
+
+        spacing
+    }
+
+    fn underline(line: &str, pos: usize) -> String {
+        let mut underline = String::new();
+
+        for c in line.chars().take(pos - 1) {
+            match c {
+                '\t' => underline.push('\t'),
+                _ => underline.push(' '),
+            }
+        }
+
+        underline.push_str("^---");
+
+        underline
+    }
+}
+
+impl Display for DelimError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.format())
     }
 }

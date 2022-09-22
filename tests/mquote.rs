fa06e8c919b543f75eabcb27ebf8d2b77e47b04a
Roman Isaikin <romix.lab@gmail.com>
2022-09-22 21:33:46 +0200

    tests: Add mquote! macro tests.

--- tests/mquote.rs
+++ tests/mquote.rs
@@ -4,9 +4,26 @@ mod test {
     use mtoken::ToTokens;
 
     #[test]
-    pub fn interpolation() {
+    pub fn interpolate() {
         let name = "MyStruct";
-        let ts = mquote!(rust r#" struct #name {} "#);
+        let ts = mquote!(rust r#" struct Λname {} "#);
+        assert_eq!(format!("{}", ts), "struct MyStruct { }")
+    }
+
+    #[test]
+    pub fn interpolate_path() {
+        struct AstNode {
+            name: String,
+        }
+        let node = AstNode { name: "MyStruct".to_owned() };
+        let ts = mquote!(rust r#" struct Λ{node.name} {} "#);
+        assert_eq!(format!("{}", ts), "struct MyStruct { }")
+    }
+
+    #[test]
+    pub fn repeat() {
+        let numbers = vec![1, 2, 3, 4, 5];
+        let ts = mquote!(rust r#" ⸨ ∀numbers ⸩,* "# debug);
         println!("{:?}", ts);
     }
 }

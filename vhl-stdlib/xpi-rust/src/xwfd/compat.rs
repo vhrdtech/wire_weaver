27cf4ee2082640f988716e53162de85c2560c881
Roman Isaikin <romix.lab@gmail.com>
2022-09-29 19:27:19 +0200

    xpi-rust: Add XwfdInfo enum that is used to distinguish between xwfd format and others.

---
+++ vhl-stdlib/xpi-rust/src/xwfd/compat.rs
@@ -0,0 +1,35 @@
+use vhl_stdlib_nostd::serdes::{DeserializeVlu4, NibbleBuf, NibbleBufMut, SerDesSize, SerializeVlu4};
+use crate::xwfd::error::XwfdError;
+
+pub enum XwfdInfo {
+    OtherFormat,
+    FormatIsXwfd,
+}
+
+impl SerializeVlu4 for XwfdInfo {
+    type Error = XwfdError;
+
+    fn ser_vlu4(&self, nwr: &mut NibbleBufMut) -> Result<(), Self::Error> {
+        let nib = match self {
+            XwfdInfo::OtherFormat => 0b1000,
+            XwfdInfo::FormatIsXwfd => 0b0000,
+        };
+        nwr.put_nibble(nib)?;
+        Ok(())
+    }
+
+    fn len_nibbles(&self) -> SerDesSize {
+        SerDesSize::Sized(1)
+    }
+}
+
+impl<'i> DeserializeVlu4<'i> for XwfdInfo {
+    type Error = XwfdError;
+
+    fn des_vlu4<'di>(nrd: &'di mut NibbleBuf<'i>) -> Result<Self, Self::Error> {
+        match nrd.get_nibble()? {
+            0b1000 => Ok(XwfdInfo::OtherFormat),
+            _ => Ok(XwfdInfo::FormatIsXwfd),
+        }
+    }
+}

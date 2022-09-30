fb1da9a472ce9b70cc22a44ee2f03084a5de5715
Roman Isaikin <romix.lab@gmail.com>
2022-09-30 19:25:47 +0200

    xpi-rust: owned: SerialUri: return xwfd::SerialUri from ser_header_xwfd(). This allows to calculate optimal uri structure only once and reuse all the code from xwfd::SerialUri.

--- vhl-stdlib/xpi-rust/src/owned/serial_uri.rs
+++ vhl-stdlib/xpi-rust/src/owned/serial_uri.rs
@@ -1,10 +1,13 @@
+use std::vec::IntoIter;
+use vhl_stdlib_nostd::discrete::{U3, U4, U6};
 use crate::owned::convert_error::ConvertError;
 use crate::xwfd;
-use vhl_stdlib_nostd::serdes::{BitBufMut, NibbleBufMut};
+use vhl_stdlib_nostd::serdes::BitBufMut;
+use vhl_stdlib_nostd::serdes::vlu4::Vlu32;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct SerialUri {
-    pub segments: Vec<u32>,
+    pub segments: Vec<Vlu32>,
 }
 
 impl SerialUri {
@@ -13,53 +16,54 @@ impl SerialUri {
     pub(crate) fn ser_header_xwfd(
         &self,
         bwr: &mut BitBufMut,
-    ) -> Result<xwfd::SerialUriDiscriminant, ConvertError> {
+    ) -> Result<xwfd::SerialUri<IntoIter<Vlu32>>, ConvertError> {
         let mut iter = self.segments.iter();
         let s03 = (iter.next(), iter.next(), iter.next(), iter.next());
-        use xwfd::SerialUriDiscriminant::*;
-        let uri_kind = match s03 {
+        use xwfd::SerialUri::*;
+        let uri = match s03 {
             (Some(s0), None, None, None) => {
-                if *s0 <= 15 {
-                    OnePart4
+                if s0.0 <= 15 {
+                    OnePart4(unsafe { U4::new_unchecked(s0.0 as u8) })
                 } else {
-                    MultiPart
+                    MultiPart(self.segments.clone().into_iter())
                 }
             }
             (Some(s0), Some(s1), None, None) => {
-                if *s0 <= 15 && *s1 <= 15 {
-                    TwoPart44
+                if s0.0 <= 15 && s1.0 <= 15 {
+                    TwoPart44(unsafe { U4::new_unchecked(s0.0 as u8) }, unsafe { U4::new_unchecked(s1.0 as u8) })
                 } else {
-                    MultiPart
+                    MultiPart(self.segments.clone().into_iter())
                 }
             }
             (Some(s0), Some(s1), Some(s2), None) => {
-                if *s0 <= 63 {
-                    if *s1 <= 63 && *s2 <= 15 {
-                        ThreePart664
-                    } else if *s1 <= 7 && *s2 <= 7 {
-                        ThreePart633
-                    } else if *s0 <= 15 && *s1 <= 15 && *s2 <= 15 {
-                        ThreePart444
+                if s0.0 <= 63 {
+                    if s1.0 <= 63 && s2.0 <= 15 {
+                        ThreePart664(unsafe { U6::new_unchecked(s0.0 as u8) }, unsafe { U6::new_unchecked(s1.0 as u8) }, unsafe { U4::new_unchecked(s2.0 as u8) })
+                    } else if s1.0 <= 7 && s2.0 <= 7 {
+                        ThreePart633(unsafe { U6::new_unchecked(s0.0 as u8) }, unsafe { U3::new_unchecked(s1.0 as u8) }, unsafe { U3::new_unchecked(s2.0 as u8) })
+                    } else if s0.0 <= 15 && s1.0 <= 15 && s2.0 <= 15 {
+                        ThreePart444(unsafe { U4::new_unchecked(s0.0 as u8) }, unsafe { U4::new_unchecked(s1.0 as u8) }, unsafe { U4::new_unchecked(s2.0 as u8) })
                     } else {
-                        MultiPart
+                        MultiPart(self.segments.clone().into_iter())
                     }
                 } else {
-                    MultiPart
+                    MultiPart(self.segments.clone().into_iter())
                 }
             }
-            (_, _, _, _) => MultiPart,
+            (_, _, _, _) => MultiPart(self.segments.clone().into_iter()),
         };
-        bwr.put_up_to_8(3, uri_kind as u8)?;
-        Ok(uri_kind)
+        bwr.put_up_to_8(3, uri.discriminant() as u8)?;
+        Ok(uri)
     }
-
-    pub(crate) fn ser_body_xwfd(
-        &self,
-        _nwr: &mut NibbleBufMut,
-        _uri_kind: xwfd::SerialUriDiscriminant,
-    ) -> Result<(), ConvertError> {
-        Ok(())
-    }
+    //
+    // pub(crate) fn ser_body_xwfd(
+    //     &self,
+    //     nwr: &mut NibbleBufMut,
+    //     uri_kind: xwfd::SerialUriDiscriminant,
+    // ) -> Result<(), ConvertError> {
+    //
+    //     Ok(())
+    // }
 }
 
 // #[derive(Clone, Debug, Eq, PartialEq)]

03bc84b7d8d02d695057bf6daec25be2b2a1668e
Roman Isaikin <romix.lab@gmail.com>
2022-10-14 21:09:06 +0200

    ast: number.rs -> auto_number, bound.rs -> num_bound.

--- ast/src/num_bound.rs
+++ ast/src/num_bound.rs
@@ -1,24 +1,12 @@
 use std::fmt::{Display, Formatter};
 use crate::ast::expr::{TryEvaluateInto, VecExpr};
-use parser::ast::num_bound::NumBound as NumBoundParser;
-use crate::ast::set::Set;
+use crate::set::Set;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum NumBound {
     Unbound,
     MaxBound(usize),
-    // Set(TryEvaluateInto<VecExpr, VecLit>),
     Set(TryEvaluateInto<VecExpr, Set>),
-}
-
-impl<'i> From<NumBoundParser<'i>> for NumBound {
-    fn from(n: NumBoundParser<'i>) -> Self {
-        match n {
-            NumBoundParser::Unbound => NumBound::Unbound,
-            NumBoundParser::MaxBound(max) => NumBound::MaxBound(max),
-            NumBoundParser::Set(exprs) => NumBound::Set(TryEvaluateInto::NotResolved(exprs.into())),
-        }
-    }
 }
 
 impl Display for NumBound {

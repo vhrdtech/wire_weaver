63182e9d0a20c76437a59d5caf5ab847331a1033
Roman Isaikin <romix.lab@gmail.com>
2022-10-14 21:09:26 +0200

    ast: Refactor Doc.

--- ast/src/doc.rs
+++ ast/src/doc.rs
@@ -8,22 +8,13 @@ pub struct Doc {
     pub lines: Vec<(Rc<String>, Span)>,
 }
 
-// impl<'i> From<ParserDoc<'i>> for Doc {
-//     fn from(pd: ParserDoc<'i>) -> Self {
-//         Doc {
-//             lines: pd
-//                 .lines
-//                 .iter()
-//                 .map(|l| (Rc::new(String::from(l.0)), l.1.clone().into()))
-//                 .collect(),
-//         }
-//     }
-// }
-
 impl Display for Doc {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        for l in &self.lines {
-            writeln!(f, "{}{}{}", color::GREEN, l.0, color::DEFAULT)?;
+        for (i, l) in self.lines.iter().enumerate() {
+            write!(f, "{}{}{}", color::GREEN, l.0, color::DEFAULT)?;
+            if i < self.lines.len() - 1 {
+                write!(f, "↩")?;
+            }
         }
         Ok(())
     }

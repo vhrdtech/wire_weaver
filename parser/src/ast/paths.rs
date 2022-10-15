3bec635cb2494ea5f938a484963f6afd5ddc5e01
Roman Isaikin <romix.lab@gmail.com>
2022-10-15 21:58:57 +0200

    parser: Refactor Path.

--- parser/src/ast/paths.rs
+++ parser/src/ast/paths.rs
@@ -1,135 +1,33 @@
+use ast::Path;
+use ast::path::ResourcePathMarker;
 use super::prelude::*;
-use crate::ast::expr::{CallArguments, IndexArguments};
-use crate::ast::naming::{Identifier, XpiUriSegmentName};
-use std::fmt::{Display, Formatter};
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
-pub enum ResourcePathKind {
-    FromRoot,
-    FromParent,
-    FromSelf,
-}
+pub struct PathParse(pub Path);
 
-impl ResourcePathKind {
-    pub fn to_str(&self) -> &'static str {
-        match self {
-            ResourcePathKind::FromRoot => "#",
-            ResourcePathKind::FromParent => "#..",
-            ResourcePathKind::FromSelf => "#.",
+pub struct ResourcePathMarkerParse(pub ResourcePathMarker);
+
+impl<'i> Parse<'i> for PathParse {
+    fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource> {
+        let simple_path = input.expect1(Rule::simple_path)?;
+        let mut input = ParseInput::fork(simple_path, input);
+        let mut segments = Vec::new();
+        while let Some(_) = input.pairs.peek() {
+            let segment: IdentifierParse<identifier::PathSegment> = input.parse()?;
+            segments.push(segment.0);
         }
+        Ok(PathParse(Path { segments }))
     }
 }
 
-impl<'i> Parse<'i> for ResourcePathKind {
+impl<'i> Parse<'i> for ResourcePathMarkerParse {
     fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource> {
         let kind = input.expect1(Rule::resource_path_start)?;
-        match kind.as_str() {
-            "#.." => Ok(ResourcePathKind::FromParent),
-            "#." => Ok(ResourcePathKind::FromSelf),
-            "#" => Ok(ResourcePathKind::FromRoot),
+        let ast_marker = match kind.as_str() {
+            "#.." => ResourcePathMarker::FromParent,
+            "#." => ResourcePathMarker::FromSelf,
+            "#" => ResourcePathMarker::FromRoot,
             _ => return Err(ParseErrorSource::internal("ResourcePathKind::parse")),
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub enum ResourcePathPart<'i> {
-    Reference(Identifier<'i, XpiUriSegmentName>),
-    IndexInto(IndexIntoResource<'i>),
-}
-
-impl<'i> Display for ResourcePathPart<'i> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ResourcePathPart::Reference(named) => write!(f, "{}", named.name),
-            ResourcePathPart::IndexInto(idx) => {
-                write!(f, "[{}", idx.name.name)?;
-                for arg in &idx.args.0 {
-                    write!(f, "{}, ", arg)?;
-                }
-                write!(f, "]")
-            }
-        }
-    }
-}
-
-impl<'i> TryFrom<ResourcePathTail<'i>> for ResourcePathPart<'i> {
-    type Error = ParseErrorSource;
-
-    fn try_from(tail: ResourcePathTail<'i>) -> Result<Self, Self::Error> {
-        match tail {
-            ResourcePathTail::Reference(named) => Ok(ResourcePathPart::Reference(named)),
-            ResourcePathTail::Call(_) => Err(ParseErrorSource::UserError),
-            ResourcePathTail::IndexInto(idx) => Ok(ResourcePathPart::IndexInto(idx)),
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub enum ResourcePathTail<'i> {
-    Reference(Identifier<'i, XpiUriSegmentName>),
-    Call(CallResource<'i>),
-    IndexInto(IndexIntoResource<'i>),
-}
-
-impl<'i> Display for ResourcePathTail<'i> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ResourcePathTail::Reference(named) => write!(f, "{}", named.name),
-            ResourcePathTail::Call(call) => write!(f, "{}", call),
-            ResourcePathTail::IndexInto(idx) => write!(f, "{}", idx),
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct CallResource<'i> {
-    pub name: Identifier<'i, XpiUriSegmentName>,
-    pub args: CallArguments<'i>,
-}
-
-impl<'i> Parse<'i> for CallResource<'i> {
-    fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource> {
-        let mut input = ParseInput::fork(input.expect1(Rule::call_expr)?, input);
-        Ok(CallResource {
-            name: input.parse()?,
-            args: input.parse()?,
-        })
-    }
-}
-
-impl<'i> Display for CallResource<'i> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}(", self.name.name)?;
-        for arg in &self.args.0 {
-            write!(f, "{}, ", arg)?;
-        }
-        write!(f, ")")
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct IndexIntoResource<'i> {
-    pub name: Identifier<'i, XpiUriSegmentName>,
-    pub args: IndexArguments<'i>,
-}
-
-impl<'i> Parse<'i> for IndexIntoResource<'i> {
-    fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource> {
-        let mut input = ParseInput::fork(input.expect1(Rule::index_into_expr)?, input);
-        Ok(IndexIntoResource {
-            name: input.parse()?,
-            args: input.parse()?,
-        })
-    }
-}
-
-impl<'i> Display for IndexIntoResource<'i> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}(", self.name.name)?;
-        for arg in &self.args.0 {
-            write!(f, "{}, ", arg)?;
-        }
-        write!(f, ")")
+        };
+        Ok(ResourcePathMarkerParse(ast_marker))
     }
 }

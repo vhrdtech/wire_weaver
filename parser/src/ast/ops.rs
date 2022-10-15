fb9a73f8d53f6c9d6232055f37b767f0b21dbf2e
Roman Isaikin <romix.lab@gmail.com>
2022-10-15 21:58:49 +0200

    parser: Refactor ops.

--- parser/src/ast/ops.rs
+++ parser/src/ast/ops.rs
@@ -1,162 +1,68 @@
 use super::prelude::*;
 
-#[derive(Debug, Eq, PartialEq, Copy, Clone)]
-pub enum BinaryOp {
-    Minus,
-    Plus,
-    Mul,
-    Div,
-    Rem,
-    BoolAnd,
-    BitAnd,
-    BoolOr,
-    BitOr,
-    Xor,
-    Lsh,
-    Rsh,
-    ClosedRange,
-    OpenRange,
-    Dot,
-    Eq,
-    Neq,
-    Gte,
-    Gt,
-    Lte,
-    Lt,
-    Path,
-}
+pub struct BinaryOpParse(pub ast::ops::BinaryOp);
 
-impl BinaryOp {
-    pub fn is_range_op(&self) -> bool {
-        *self == BinaryOp::ClosedRange || *self == BinaryOp::OpenRange
-    }
+pub struct UnaryOpParse(pub ast::ops::UnaryOp);
 
-    pub fn from_rule(rule: Rule) -> Result<Self, ParseErrorSource> {
-        match rule {
-            Rule::op_plus => Ok(BinaryOp::Plus),
-            Rule::op_minus => Ok(BinaryOp::Minus),
-            Rule::op_mul => Ok(BinaryOp::Mul),
-            Rule::op_div => Ok(BinaryOp::Div),
-            Rule::op_rem => Ok(BinaryOp::Rem),
-            Rule::op_bool_and => Ok(BinaryOp::BoolAnd),
-            Rule::op_bit_and => Ok(BinaryOp::BitAnd),
-            Rule::op_bool_or => Ok(BinaryOp::BoolOr),
-            Rule::op_bit_or => Ok(BinaryOp::BitOr),
-            Rule::op_xor => Ok(BinaryOp::Xor),
-            Rule::op_lsh => Ok(BinaryOp::Lsh),
-            Rule::op_rsh => Ok(BinaryOp::Rsh),
-            Rule::op_closed_range => Ok(BinaryOp::ClosedRange),
-            Rule::op_open_range => Ok(BinaryOp::OpenRange),
-            Rule::op_dot => Ok(BinaryOp::Dot),
-            Rule::op_eq => Ok(BinaryOp::Eq),
-            Rule::op_neq => Ok(BinaryOp::Neq),
-            Rule::op_gte => Ok(BinaryOp::Gte),
-            Rule::op_gt => Ok(BinaryOp::Gt),
-            Rule::op_lte => Ok(BinaryOp::Lte),
-            Rule::op_lt => Ok(BinaryOp::Lt),
-            Rule::op_path => Ok(BinaryOp::Path),
-            _ => Err(ParseErrorSource::internal("expected op_binary")),
-        }
-    }
-
-    pub fn binding_power(&self) -> (u8, u8) {
-        use BinaryOp::*;
-        match self {
-            OpenRange | ClosedRange => (1, 2),
-            BoolOr => (3, 4),
-            BoolAnd => (5, 6),
-            Eq | Neq | Gte | Gt | Lte | Lt => (7, 8),
-            BitOr => (9, 10),
-            Xor => (11, 12),
-            BitAnd => (13, 14),
-            Lsh | Rsh => (15, 16),
-            Plus | Minus => (17, 18),
-            Mul | Div | Rem => (19, 20),
-            Dot => (21, 22),
-            Path => (23, 24),
-        }
-    }
-
-    pub fn to_str(&self) -> &'static str {
-        match self {
-            BinaryOp::Minus => "-",
-            BinaryOp::Plus => "+",
-            BinaryOp::Mul => "*",
-            BinaryOp::Div => "/",
-            BinaryOp::Rem => "%",
-            BinaryOp::BoolAnd => "&&",
-            BinaryOp::BitAnd => "&",
-            BinaryOp::BoolOr => "||",
-            BinaryOp::BitOr => "|",
-            BinaryOp::Xor => "^",
-            BinaryOp::Lsh => "<<",
-            BinaryOp::Rsh => ">>",
-            BinaryOp::ClosedRange => "..=",
-            BinaryOp::OpenRange => "..",
-            BinaryOp::Dot => ".",
-            BinaryOp::Eq => "==",
-            BinaryOp::Neq => "!=",
-            BinaryOp::Gte => ">=",
-            BinaryOp::Gt => ">",
-            BinaryOp::Lte => "<=",
-            BinaryOp::Lt => "<",
-            BinaryOp::Path => "::",
-        }
+pub fn binary_from_rule(rule: Rule) -> Result<ast::ops::BinaryOp, ParseErrorSource> {
+    use ast::ops::BinaryOp::*;
+    match rule {
+        Rule::op_plus => Ok(Plus),
+        Rule::op_minus => Ok(Minus),
+        Rule::op_mul => Ok(Mul),
+        Rule::op_div => Ok(Div),
+        Rule::op_rem => Ok(Rem),
+        Rule::op_bool_and => Ok(BoolAnd),
+        Rule::op_bit_and => Ok(BitAnd),
+        Rule::op_bool_or => Ok(BoolOr),
+        Rule::op_bit_or => Ok(BitOr),
+        Rule::op_xor => Ok(Xor),
+        Rule::op_lsh => Ok(Lsh),
+        Rule::op_rsh => Ok(Rsh),
+        Rule::op_closed_range => Ok(ClosedRange),
+        Rule::op_open_range => Ok(OpenRange),
+        Rule::op_dot => Ok(Dot),
+        Rule::op_eq => Ok(Eq),
+        Rule::op_neq => Ok(Neq),
+        Rule::op_gte => Ok(Gte),
+        Rule::op_gt => Ok(Gt),
+        Rule::op_lte => Ok(Lte),
+        Rule::op_lt => Ok(Lt),
+        Rule::op_path => Ok(Path),
+        _ => Err(ParseErrorSource::internal("expected op_binary")),
     }
 }
 
-impl<'i> Parse<'i> for BinaryOp {
+pub fn unary_from_rule(rule: Rule) -> Result<ast::ops::UnaryOp, ParseErrorSource> {
+    use ast::ops::UnaryOp::*;
+    match rule {
+        Rule::op_minus => Ok(Minus),
+        Rule::op_plus => Ok(Plus),
+        Rule::op_not => Ok(Not),
+        _ => Err(ParseErrorSource::internal("expected op_unary rule")),
+    }
+}
+
+impl<'i> Parse<'i> for BinaryOpParse {
     fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource> {
         let op = input.expect1(Rule::op_binary)?;
-        Ok(BinaryOp::from_rule(
+        Ok(BinaryOpParse(binary_from_rule(
             op.into_inner()
                 .next()
                 .ok_or_else(|| ParseErrorSource::internal("wrong op_binary rule"))?
                 .as_rule(),
-        )?)
+        )?))
     }
 }
 
-#[derive(Debug, Clone, Eq, PartialEq)]
-pub enum UnaryOp {
-    Minus,
-    Plus,
-    Not,
-}
-
-impl UnaryOp {
-    pub fn from_rule(rule: Rule) -> Result<Self, ParseErrorSource> {
-        match rule {
-            Rule::op_minus => Ok(UnaryOp::Minus),
-            Rule::op_plus => Ok(UnaryOp::Plus),
-            Rule::op_not => Ok(UnaryOp::Not),
-            _ => Err(ParseErrorSource::internal("expected op_unary rule")),
-        }
-    }
-
-    pub fn binding_power(&self) -> ((), u8) {
-        match self {
-            UnaryOp::Plus | UnaryOp::Minus | UnaryOp::Not => ((), 23),
-        }
-    }
-
-    pub fn to_str(&self) -> &'static str {
-        match self {
-            UnaryOp::Minus => "-",
-            UnaryOp::Plus => "+",
-            UnaryOp::Not => "!",
-        }
-    }
-}
-
-impl<'i> Parse<'i> for UnaryOp {
+impl<'i> Parse<'i> for UnaryOpParse {
     fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource> {
         let op = input.expect1(Rule::op_unary)?;
-        Ok(UnaryOp::from_rule(
+        Ok(UnaryOpParse(unary_from_rule(
             op.into_inner()
                 .next()
                 .ok_or_else(|| ParseErrorSource::internal("wrong op_unary rule"))?
                 .as_rule(),
-        )?)
+        )?))
     }
 }

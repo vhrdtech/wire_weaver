371ec9a9143018136de8ff945d422ffc3fdec245
Roman Isaikin <romix.lab@gmail.com>
2022-10-15 21:57:11 +0200

    parser: Refactor FnDef.

--- parser/src/ast/def_fn.rs
+++ parser/src/ast/def_fn.rs
@@ -1,90 +1,59 @@
+use ast::{FnArg, FnArguments, FnDef};
 use super::prelude::*;
-use crate::ast::generics::Generics;
-use crate::ast::naming::{FnArgName, FnName};
-use crate::ast::stmt::Stmt;
-use crate::ast::ty::Ty;
+use crate::ast::generics::GenericsParse;
+use crate::ast::stmt::VecStmtParse;
+use crate::ast::ty::TyParse;
 
-#[derive(Debug, Clone)]
-pub struct DefFn<'i> {
-    pub docs: Doc<'i>,
-    pub attrs: Attrs<'i>,
-    pub name: Identifier<'i, FnName>,
-    pub generics: Option<Generics<'i>>,
-    pub arguments: FnArguments<'i>,
-    pub ret_ty: Option<FnRetTy<'i>>,
-    pub statements: FnStmts<'i>,
-}
+pub struct FnDefParse(pub FnDef);
 
-impl<'i> Parse<'i> for DefFn<'i> {
+pub struct FnArgumentsParse(pub FnArguments);
+
+pub struct FnArgParse(pub FnArg);
+
+impl<'i> Parse<'i> for FnDefParse {
     fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource> {
         let mut input = ParseInput::fork(input.expect1(Rule::def_fn)?, input);
-
-        Ok(DefFn {
-            docs: input.parse()?,
-            attrs: input.parse()?,
-            name: input.parse()?,
-            generics: input.parse_or_skip()?,
-            arguments: input.parse()?,
-            ret_ty: input.parse_or_skip()?,
-            statements: input.parse()?,
-        })
+        let doc: DocParse = input.parse()?;
+        let attrs: AttrsParse = input.parse()?;
+        let name: IdentifierParse<identifier::FnName> = input.parse()?;
+        let generics: Option<GenericsParse> = input.parse_or_skip()?;
+        let arguments: FnArgumentsParse = input.parse()?;
+        let ret_ty: Option<TyParse> = input.parse_or_skip()?;
+        let statements: VecStmtParse = input.parse()?;
+        Ok(FnDefParse(FnDef {
+            doc: doc.0,
+            attrs: attrs.0,
+            name: name.0,
+            generics: generics.map(|g| g.0),
+            arguments: arguments.0,
+            ret_ty: ret_ty.map(|ty| ty.0),
+            statements: statements.0,
+        }))
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct FnArguments<'i> {
-    pub args: Vec<FnArg<'i>>,
-}
 
-impl<'i> Parse<'i> for FnArguments<'i> {
+impl<'i> Parse<'i> for FnArgumentsParse {
     fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource> {
         let mut input = ParseInput::fork(input.expect1(Rule::fn_args)?, input);
 
         let mut args = Vec::new();
         while let Some(_) = input.pairs.peek() {
-            args.push(input.parse()?);
+            let fn_arg: FnArgParse = input.parse()?;
+            args.push(fn_arg.0);
         }
-        Ok(FnArguments { args })
+        Ok(FnArgumentsParse(FnArguments { args }))
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct FnArg<'i> {
-    pub name: Identifier<'i, FnArgName>,
-    pub ty: Ty<'i>,
-}
-
-impl<'i> Parse<'i> for FnArg<'i> {
+impl<'i> Parse<'i> for FnArgParse {
     fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource> {
         let mut input = ParseInput::fork(input.expect1(Rule::named_ty)?, input);
-        Ok(FnArg {
-            name: input.parse()?,
-            ty: input.parse()?,
-        })
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct FnStmts<'i> {
-    pub stmts: Vec<Stmt<'i>>,
-}
-
-impl<'i> Parse<'i> for FnStmts<'i> {
-    fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource> {
-        let mut stmts = Vec::new();
-        while let Some(_) = input.pairs.peek() {
-            stmts.push(input.parse()?);
-        }
-        Ok(FnStmts { stmts })
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct FnRetTy<'i>(pub Ty<'i>);
-
-impl<'i> Parse<'i> for FnRetTy<'i> {
-    fn parse<'m>(input: &mut ParseInput<'i, 'm>) -> Result<Self, ParseErrorSource> {
-        let mut input = ParseInput::fork(input.expect1(Rule::fn_ret_ty)?, input);
-        Ok(FnRetTy(input.parse()?))
+        let name: IdentifierParse<identifier::FnArgName> = input.parse()?;
+        let ty: TyParse = input.parse()?;
+        Ok(FnArgParse(FnArg {
+            name: name.0,
+            ty: ty.0,
+        }))
     }
 }

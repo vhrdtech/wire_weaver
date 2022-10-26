33d7b5b58b5278ac7fcb4195176da25e1019ae1a
Roman Isaikin <romix.lab@gmail.com>
2022-10-26 22:30:26 +0200

    codegen: size_in_byte_buf(): resolve user types in context of a project at a specific path.

--- codegen/src/rust/serdes/buf/size.rs
+++ codegen/src/rust/serdes/buf/size.rs
@@ -1,13 +1,15 @@
-use ast::{NumBound, TryEvaluateInto, Ty, TyKind};
+use ast::{NumBound, Path, TryEvaluateInto, Ty, TyKind};
+use vhl::project::Project;
 use vhl_stdlib::serdes::SerDesSize;
+use crate::error::CodegenError;
 
-pub fn size_in_buf(of: &Ty) -> SerDesSize {
+pub fn size_in_byte_buf(of: &Ty, at_path: &Path, _project: &Project) -> Result<SerDesSize, CodegenError> {
     match &of.kind {
-        TyKind::Unit => SerDesSize::Sized(0),
-        TyKind::Boolean => SerDesSize::Sized(1),
+        TyKind::Unit => Ok(SerDesSize::Sized(0)),
+        TyKind::Boolean => Ok(SerDesSize::Sized(1)),
         TyKind::Discrete(discrete) => {
             if discrete.is_standard() {
-                SerDesSize::Sized(discrete.bits as usize / 8)
+                Ok(SerDesSize::Sized(discrete.bits as usize / 8))
             } else {
                 todo!()
             }
@@ -16,22 +18,27 @@ pub fn size_in_buf(of: &Ty) -> SerDesSize {
         TyKind::Float(_) => todo!(),
         TyKind::Array { .. } => todo!(),
         TyKind::Tuple { .. } => todo!(),
-        TyKind::Char => SerDesSize::UnsizedBound(4), // UTF8 codepoint
+        TyKind::Char => Ok(SerDesSize::UnsizedBound(4)), // UTF8 codepoint
         TyKind::String { len_bound } => match len_bound {
-            NumBound::Unbound | NumBound::MinBound(_) => SerDesSize::Unsized,
+            NumBound::Unbound | NumBound::MinBound(_) => Ok(SerDesSize::Unsized),
             NumBound::MaxBound(_max) => todo!(),
             NumBound::Set(set) => match set {
-                TryEvaluateInto::Resolved(set) => SerDesSize::UnsizedBound(set.max_len()),
+                TryEvaluateInto::Resolved(set) => Ok(SerDesSize::UnsizedBound(set.max_len())),
                 TryEvaluateInto::NotResolved(_) | TryEvaluateInto::Error => {
-                    panic!("internal: not processed AST is given to codegen")
+                    return Err(CodegenError::Internal("size_in_byte_buf: not processed AST is given to codegen".to_owned()));
                 }
             },
         },
-        TyKind::UserDefined(_) => todo!(), // need to resolve first
+        TyKind::UserDefined(user_path) => {
+            println!("size_in_byte_buf of: {} at: {}", user_path, at_path);
+            Ok(SerDesSize::Unsized)
+        },
         TyKind::Derive
         | TyKind::Fn { .. }
         | TyKind::AutoNumber(_)
         | TyKind::IndexTyOf(_)
-        | TyKind::Generic { .. } => panic!("internal: invalid AST is given to codegen"),
+        | TyKind::Generic { .. } => {
+            return Err(CodegenError::Internal("size_in_byte_buf: wrong AST is given to codegen".to_owned()));
+        },
     }
 }

7112aec301f86abbecab0863a32af6343785d838
Isaikin Roman <romix.lab@gmail.com>
2023-07-10 18:34:57 +0200

    xpi: Implement PartialEq for Nrl with slices and arrays.

--- vhl-stdlib-rust/xpi/src/client_server_owned/nrl.rs
+++ vhl-stdlib-rust/xpi/src/client_server_owned/nrl.rs
@@ -1,4 +1,4 @@
-use core::fmt::Display;
+use core::{fmt::Display, ops::Deref};
 
 use smallvec::SmallVec;
 
@@ -19,6 +19,26 @@ impl Nrl {
         self.0.iter()
     }
 }
+
+impl PartialEq<[u32]> for Nrl {
+    fn eq(&self, other: &[u32]) -> bool {
+        self.0.deref() == other
+    }
+}
+
+macro_rules! impl_partial_eq {
+    ($len:literal) => {
+        impl PartialEq<[u32; $len]> for Nrl {
+            fn eq(&self, other: &[u32; $len]) -> bool {
+                self.0.deref() == other
+            }
+        }
+    };
+}
+impl_partial_eq!(1);
+impl_partial_eq!(2);
+impl_partial_eq!(3);
+impl_partial_eq!(4);
 
 impl Display for Nrl {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {

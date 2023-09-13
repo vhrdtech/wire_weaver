013b44adf400c655ca4fd8cb2dc3a36f4661b85d
Isaikin Roman <romix.lab@gmail.com>
2023-09-13 08:21:52 +0200

    stdlib-rust: xpi_client_server: Accept impl Iterator in StreamResult.

--- vhl-stdlib-rust/xpi-client-server/src/server/stream_result.rs
+++ vhl-stdlib-rust/xpi-client-server/src/server/stream_result.rs
@@ -3,8 +3,10 @@ use futures_util::SinkExt;
 use serde::Serialize;
 use std::marker::PhantomData;
 use thiserror::Error;
-use tracing::{error, warn};
+use tracing::error;
 use xpi::client_server_owned::{AddressableEvent, Event, Nrl, Protocol, RequestId};
+use crate::util::IteratorAdapter;
+
 
 pub struct StreamResultContext<T> {
     pub source: Protocol,
@@ -23,32 +25,28 @@ pub enum PublishError {
     MpscError,
 }
 
-impl<T: Serialize> StreamResultContext<T> {
-    pub fn new(
-        source: Protocol,
-        nrl: Nrl,
-        seq: RequestId,
-        events_tx: Sender<AddressableEvent>,
-    ) -> Self {
+impl<'a, T: Serialize + 'a> StreamResultContext<T> {
+    pub fn new(request: &AddressableEvent) -> Self {
         StreamResultContext {
-            source,
-            nrl,
-            seq,
-            events_tx,
+            source: request.protocol,
+            nrl: request.event.nrl.clone(),
+            seq: request.event.seq,
+            events_tx: request.response_tx.clone(),
             _phantom: PhantomData {},
         }
     }
 
-    pub async fn publish_one(&mut self, item: T) -> Result<(), PublishError> {
-        self.publish_many(&[item]).await
+    pub async fn publish_one(&mut self, item: &'a T) -> Result<(), PublishError> {
+        self.publish_many([item].into_iter()).await
     }
 
-    pub async fn publish_many(&mut self, items: &[T]) -> Result<(), PublishError> {
-        if items.is_empty() {
-            return Ok(());
-        }
+    pub async fn publish_many(&mut self, items: impl Iterator<Item = &'a T>) -> Result<(), PublishError> {
+        // if items.is_empty() {
+        //     return Ok(());
+        // }
         let mut data = Vec::new();
-        serde::Serialize::serialize(items, &mut rmp_serde::Serializer::new(&mut data))?;
+        let iterator_adapter = IteratorAdapter::new(items);
+        serde::Serialize::serialize(&iterator_adapter, &mut rmp_serde::Serializer::new(&mut data))?;
         let ev = Event::stream_update(self.nrl.clone(), data, self.seq);
         self.events_tx
             .send(AddressableEvent {
--- vhl-stdlib-rust/xpi-client-server/src/util.rs
+++ vhl-stdlib-rust/xpi-client-server/src/util.rs
@@ -1,9 +1,31 @@
 use futures::Stream;
 use std::time::Duration;
+use std::cell::RefCell;
 
 pub fn tick_stream(period: Duration) -> impl Stream<Item = ()> {
     futures::stream::unfold(period, move |p| async move {
         tokio::time::sleep(period).await;
         Some(((), p))
     })
+}
+
+pub struct IteratorAdapter<I>(pub RefCell<I>);
+
+impl<I> IteratorAdapter<I> {
+    pub fn new(iterator: I) -> Self {
+        Self(RefCell::new(iterator))
+    }
+}
+
+impl<I> serde::Serialize for IteratorAdapter<I>
+    where
+        I: Iterator,
+        I::Item: serde::Serialize,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer,
+    {
+        serializer.collect_seq(self.0.borrow_mut().by_ref())
+    }
 }

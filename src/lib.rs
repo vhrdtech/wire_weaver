d8e0b37f3653c72fd6c36241c91a4a573ef4ac59
Roman Isaikin <romix.lab@gmail.com>
2025-02-10 20:12:09 +0100

    refactor: use

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
-use std::thread::sleep;
-use std::time::Duration;
 use nusb::{Interface};
 use nusb::transfer::{RequestBuffer, TransferError};
 use tokio::sync::mpsc::{channel, Receiver, Sender};
 use tokio::sync::mpsc::error::TryRecvError;
 use tracing::{error, info, trace, warn};
-use wire_weaver_usb_link::{PacketSender, PacketReceiver, FrameSink, FrameSource, LinkMgmtCmd, ProtocolInfo, PacketKind, ReceiveError};
+use wire_weaver_usb_link::{WireWeaverUsbLink, MessageKind, PacketSink, PacketSource, ProtocolInfo, Error as LinkError};
 
 pub enum DeviceManagerCommand {
     Open {
@@ -133,10 +131,10 @@ struct Sink {
     // vec to reuse
 }
 
-impl FrameSink for Sink {
+impl PacketSink for Sink {
     type Error = TransferError;
 
-    async fn write_frame(&mut self, data: &[u8]) -> Result<(), Self::Error> {
+    async fn write_packet(&mut self, data: &[u8]) -> Result<(), Self::Error> {
         // Try Queue out
         let completion = self.interface.interrupt_out(0x01, data.to_vec()).await;
         match completion.status {
@@ -151,18 +149,6 @@ impl FrameSink for Sink {
         }
         // let reuse_vec = completion.data.reuse();
     }
-
-    async fn wait_connection(&mut self) {
-
-    }
-
-    async fn rx_from_source(&mut self) -> LinkMgmtCmd {
-        unreachable!()
-    }
-
-    fn try_rx_from_source(&mut self) -> Option<LinkMgmtCmd> {
-        None
-    }
 }
 
 struct Source {
@@ -170,10 +156,10 @@ struct Source {
     // vec to reuse
 }
 
-impl FrameSource for Source {
+impl PacketSource for Source {
     type Error = TransferError;
 
-    async fn read_frame(&mut self, data: &mut [u8]) -> Result<usize, Self::Error> {
+    async fn read_packet(&mut self, data: &mut [u8]) -> Result<usize, Self::Error> {
         // reuse vec
         // try Queue in
         let completion = self.interface.interrupt_in(0x81, RequestBuffer::new(512)).await;
@@ -189,14 +175,6 @@ impl FrameSource for Source {
             }
         }
     }
-
-    async fn wait_connection(&mut self) {
-
-    }
-
-    fn send_to_sink(&mut self, _msg: LinkMgmtCmd) {
-
-    }
 }
 
 async fn worker(
@@ -209,65 +187,73 @@ async fn worker(
         major_version: 0,
         minor_version: 1,
     };
-    let mut builder_buf = [0u8; 1024];
-    let mut packet_sender = PacketSender::new(&mut builder_buf, Sink { interface: interface.clone() }, user_protocol);
-    const MAX_PACKET_SIZE: usize = 2048;
-    // for some reason first packet seem to be lost when connecting for the second tmie, so send nop
-    packet_sender.send_nop().await.unwrap();
-    packet_sender.send_link_setup(MAX_PACKET_SIZE as u32).await.unwrap();
+    // TODO: use 1024 USB transfers?
+    let mut tx_buf = [0u8; 512];
+    let mut rx_buf = [0u8; 512];
+    let mut link = WireWeaverUsbLink::new(user_protocol, Sink { interface: interface.clone() }, &mut tx_buf, Source { interface }, &mut rx_buf);
+    const MAX_MESSAGE_SIZE: usize = 2048;
+    let mut message_rx = [0u8; MAX_MESSAGE_SIZE];
 
-    tokio::spawn(async move {
-        let mut reader_staging = [0u8; 512]; // max USB transfer size, TODO: use 1024 USB transfers?
-        let mut reader_rx = [0u8; MAX_PACKET_SIZE];
-        let mut packet_receiver = PacketReceiver::new(Source { interface }, &mut reader_staging, user_protocol);
-        loop {
-            match packet_receiver.receive_packet(&mut reader_rx).await {
-                Ok(PacketKind::Data(len)) => {
-                    let packet = &reader_rx[..len];
-                    info!("Packet: {packet:02x?}");
-                }
-                Ok(PacketKind::Disconnect) => {
-                    info!("Received Disconnect, exiting");
-                    break;
-                }
-                Ok(PacketKind::Ping) => {
-                    trace!("Ping");
-                }
-                Ok(PacketKind::LinkInfo { remote_max_packet_size, remote_user_protocol }) => {
-                    info!("Link info: protocol {remote_user_protocol:?} max packet size: {remote_max_packet_size}");
-                }
-                Err(ReceiveError::EmptyFrame) => {
-                    info!("Receive empty frame");
-                    // break;
-                }
-                Err(ReceiveError::ProtocolsVersionsMismatch) => {
-                    warn!("Protocols versions mismatch");
-                }
-                Err(ReceiveError::SourceError(e)) => {
-                    error!("Transfer error: {e:?}, exiting");
-                    break;
-                }
-                Err(ReceiveError::InternalBufOverflow) => {
-                    warn!("Internal buf overflow while receiving packet");
-                }
-            }
-
-        }
-    });
+    // for some reason first packet seem to be lost when connecting for the second time, so send nop
+    link.send_nop().await.unwrap();
+    link.send_link_setup(MAX_MESSAGE_SIZE as u32).await.unwrap();
 
     loop {
         tokio::select! {
+            message = link.receive_message(&mut message_rx) => {
+                match message {
+                    Ok(MessageKind::Data(len)) => {
+                        let packet = &message_rx[..len];
+                        info!("Packet: {packet:02x?}");
+                        tx_events.send(Event::Received(packet.to_vec())).await.unwrap();
+                    }
+                    Ok(MessageKind::Disconnect) => {
+                        info!("Received Disconnect, exiting");
+                        break;
+                    }
+                    Ok(MessageKind::Ping) => {
+                        trace!("Ping");
+                    }
+                    Ok(MessageKind::LinkInfo { remote_max_message_size, remote_protocol }) => {
+                        info!("Link info: protocol {remote_protocol:?} max packet size: {remote_max_message_size}");
+                    }
+                    Err(LinkError::ReceivedEmptyPacket) => {
+                        info!("Receive empty frame");
+                        // break;
+                    }
+                    Err(LinkError::ProtocolsVersionMismatch) => {
+                        warn!("Protocols versions mismatch");
+                    }
+                    Err(LinkError::InternalBufOverflow) => {
+                        warn!("Internal buf overflow while receiving packet");
+                    }
+                    Err(LinkError::LinkVersionMismatch) => {
+                        warn!("Link versions mismatch");
+                    }
+                    Err(LinkError::SourceError(e)) => {
+                        error!("Transfer error: {e:?}, exiting");
+                        break;
+                    }
+                    Err(LinkError::SinkError(e)) => {
+                        error!("USB error: {e:?}, exiting");
+                        break;
+                    }
+                    Err(e) => {
+                        error!("Link error: {e:?}");
+                    }
+                }
+            }
             cmd = rx_commands.recv() => {
                 match cmd {
                     Some(Command::Send(buf)) => {
                         info!("Sending data {buf:?}");
-                        packet_sender.send_packet(&buf).await;
-                        packet_sender.force_send().await; // TODO: force send on timer
+                        link.send_message(&buf).await.unwrap();
+                        link.force_send().await.unwrap(); // TODO: force send on timer
                     }
                     Some(Command::TestLink) => {
                         // let mut packet_builder = FrameBuilder::new(&mut tx_assembly_buf);
                     }
-                    Some(Command::RecycleBuffer(buf)) => {
+                    Some(Command::RecycleBuffer(_buf)) => {
 
                     }
                     Some(Command::Close) => {
@@ -281,6 +267,7 @@ async fn worker(
                         if r.is_err() {
                             warn!("Channel closed");
                         }
+                        link.send_disconnect().await.unwrap();
                         break;
                     }
                 }

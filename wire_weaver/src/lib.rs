0de79629cc970c2486b2dd7e4be34b4c74cd19f2
Roman Isaikin <romix.lab@gmail.com>
2025-03-15 20:31:50 +0100

    refactor: move ProtocolInfo to wire_weaver crate.

--- wire_weaver/src/lib.rs
+++ wire_weaver/src/lib.rs
@@ -1,4 +1,54 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub use shrink_wrap;
-pub use wire_weaver_derive::{wire_weaver, wire_weaver_api, ShrinkWrap};
+use shrink_wrap::{BufReader, BufWriter};
+pub use wire_weaver_derive::{ShrinkWrap, wire_weaver_api, ww_repr};
+
+/// User protocol ID and version. Only major and minor numbers are used and checked.
+/// Protocols are compatible if IDs are equal and if major versions matches for major >= 1.
+/// So all 1.x and 1.y series are considered compatible, so that older firmwares can talk to newer
+/// host software and older host software can talk to newer firmwares.
+///
+/// If major == 0, then only minor versions are compared. I.e. 0.1 and 0.2 are incompatible and can
+/// be used during development.
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct ProtocolInfo {
+    pub protocol_id: u32,
+    pub major_version: u8,
+    pub minor_version: u8,
+    // TODO: Add patch version?
+}
+
+impl ProtocolInfo {
+    pub const fn size_bytes() -> usize {
+        6
+    }
+
+    // TODO: use derive(shrink_wrap)
+    pub fn write(&self, wr: &mut BufWriter) -> Result<(), shrink_wrap::Error> {
+        wr.write_u32(self.protocol_id)?;
+        wr.write_u8(self.major_version)?;
+        wr.write_u8(self.minor_version)?;
+        Ok(())
+    }
+
+    pub fn read(rd: &mut BufReader) -> Result<ProtocolInfo, shrink_wrap::Error> {
+        Ok(ProtocolInfo {
+            protocol_id: rd.read_u32()?,
+            major_version: rd.read_u8()?,
+            minor_version: rd.read_u8()?,
+        })
+    }
+
+    pub fn is_compatible(&self, other: &ProtocolInfo) -> bool {
+        if self.protocol_id != other.protocol_id {
+            false
+        } else if self.major_version == 0 && other.major_version == 0 {
+            self.minor_version == other.minor_version
+        } else {
+            // not comparing minor versions, protocols are supposed to be backwards and forwards compatible after 1.0
+            self.major_version == other.major_version
+        }
+    }
+}

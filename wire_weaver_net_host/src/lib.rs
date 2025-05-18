63e54fabd1eea8916d2c1494fa502aee007902d4
Roman Isaikin <romix.lab@gmail.com>
2025-05-18 18:47:03 +0200

    fix: export wire_weaver_client_server

--- wire_weaver_net_host/src/lib.rs
+++ wire_weaver_net_host/src/lib.rs
@@ -3,5 +3,6 @@ mod event_loop_ws;
 
 pub use event_loop_udp::{UdpError, UdpTarget, udp_worker};
 pub use event_loop_ws::{WsError, WsTarget, ws_worker};
+pub use wire_weaver_client_server;
 pub use wire_weaver_client_server::util;
 pub use wire_weaver_client_server::{Command, Error, OnError};

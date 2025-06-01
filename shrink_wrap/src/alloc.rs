241a2119d0cf1e9f5140b2ea61c120463ee599e0
Roman Isaikin <romix.lab@gmail.com>
2025-06-01 12:27:51 +0200

    feat: ShrinkWrap traits for String.

--- shrink_wrap/src/alloc.rs
+++ shrink_wrap/src/alloc.rs
@@ -31,3 +31,18 @@ impl<'i, T: DeserializeShrinkWrap<'i>> DeserializeShrinkWrap<'i> for Vec<T> {
         Ok(items)
     }
 }
+
+impl SerializeShrinkWrap for String {
+    fn ser_shrink_wrap(&self, wr: &mut BufWriter) -> Result<(), Error> {
+        wr.write_raw_str(self.as_str())
+    }
+}
+
+impl<'i> DeserializeShrinkWrap<'i> for String {
+    fn des_shrink_wrap<'di>(
+        rd: &'di mut BufReader<'i>,
+        _element_size: ElementSize,
+    ) -> Result<Self, Error> {
+        Ok(String::from(rd.read_raw_str()?))
+    }
+}

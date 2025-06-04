b14f7d9bd7c8de3d8b62a14484067ba1d21ea8b5
Roman Isaikin <romix.lab@gmail.com>
2025-06-04 22:24:59 +0200

    feat: convenience methods: to_slice and from_slice.

--- wire_weaver/src/lib.rs
+++ wire_weaver/src/lib.rs
@@ -1,14 +1,32 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod date_time;
+mod test;
 pub mod version;
 
 pub use shrink_wrap;
-use shrink_wrap::{BufReader, BufWriter};
+use shrink_wrap::{BufReader, BufWriter, DeserializeShrinkWrap, SerializeShrinkWrap};
 pub use wire_weaver_derive::{derive_shrink_wrap, wire_weaver_api, ww_repr};
 
 #[cfg(feature = "chrono")]
 pub use chrono;
+
+pub fn to_slice<'i, T: SerializeShrinkWrap>(
+    buf: &'i mut [u8],
+    value: &T,
+) -> Result<&'i [u8], shrink_wrap::Error> {
+    let mut wr = BufWriter::new(buf);
+    value.ser_shrink_wrap(&mut wr)?;
+    wr.finish_and_take()
+}
+
+pub fn from_slice<'i, T: DeserializeShrinkWrap<'i>>(
+    buf: &'i [u8],
+) -> Result<T, shrink_wrap::Error> {
+    let mut rd = BufReader::new(buf);
+    let value = T::des_shrink_wrap(&mut rd)?;
+    Ok(value)
+}
 
 /// User protocol ID and version. Only major and minor numbers are used and checked.
 /// Protocols are compatible if IDs are equal and if major versions matches for major >= 1.

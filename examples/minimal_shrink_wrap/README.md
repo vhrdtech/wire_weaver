1ac9b47cec42173b07901413cb80f62138d535a3
Roman Isaikin <romix.lab@gmail.com>
2025-11-20 22:07:29 +0100

    feat: minimal_shrink_wrap example

---
+++ examples/minimal_shrink_wrap/README.md
@@ -0,0 +1,5 @@
+# minimal_shrink_wrap
+
+This is a small example on how to use ShrinkWrap wire format directly. See the
+[documentation page](https://vhrdtech.github.io/wire_weaver/serdes/shrink_wrap/) for an in-depth wire format
+description.
--- examples/minimal_shrink_wrap/src/main.rs
+++ examples/minimal_shrink_wrap/src/main.rs
@@ -1,3 +1,22 @@
+use wire_weaver::shrink_wrap::prelude::*;
+
+fn simple_wr() {
+    let mut buf = [0u8; 256];
+    let mut wr = BufWriter::new(&mut buf);
+    wr.write_bool(true).unwrap();
+    wr.write_u8(0xaa).unwrap();
+    let bytes = wr.finish().unwrap();
+    assert_eq!(bytes, &[0x80, 0xaa]);
+}
+
+fn simple_rd() {
+    let buf = [0x80, 0xaa];
+    let mut rd = BufReader::new(&buf[..]);
+    assert!(rd.read_bool().unwrap());
+    assert_eq!(rd.read_u8().unwrap(), 0xaa);
+}
+
 fn main() {
-    println!("Hello, world!");
+    simple_wr();
+    simple_rd();
 }

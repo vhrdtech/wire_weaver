bee25d63239806ea73cb06dcfab53701c0075118
Roman Isaikin <romix.lab@gmail.com>
2026-01-15 11:20:24 +0000

    refactor: do not generate method output structs and DeserializeShrinkWrapOwned in api code

--- wire_weaver_core/src/codegen/api_common.rs
+++ wire_weaver_core/src/codegen/api_common.rs
@@ -1,22 +1,17 @@
 use crate::ast::api::{ApiItemKind, ApiLevel};
 use convert_case::Casing;
-use proc_macro2::{Ident, Span, TokenStream};
+use proc_macro2::{Ident, TokenStream};
 use quote::TokenStreamExt;
-use shrink_wrap_core::ast::{Docs, Field, ItemStruct, Type};
+use shrink_wrap_core::ast::{Docs, Field, ItemStruct};
 use shrink_wrap_core::transform::create_flags;
 
 pub fn args_structs(api_level: &ApiLevel, no_alloc: bool) -> TokenStream {
     let mut defs = TokenStream::new();
     for item in &api_level.items {
-        if let ApiItemKind::Method {
-            ident,
-            args,
-            return_type,
-        } = &item.kind
-        {
-            if let Some(ty) = return_type {
-                output_struct(&mut defs, ident, ty, no_alloc);
-            }
+        if let ApiItemKind::Method { ident, args, .. } = &item.kind {
+            // if let Some(ty) = return_type {
+            //     output_struct(&mut defs, ident, ty, no_alloc);
+            // }
 
             let mut fields = vec![];
             for (id, arg) in args.iter().enumerate() {
@@ -49,38 +44,38 @@ pub fn args_structs(api_level: &ApiLevel, no_alloc: bool) -> TokenStream {
                 size_assumption: None,
             };
             defs.append_all(item_struct.def_rust(no_alloc));
-            defs.append_all(item_struct.serdes_rust(no_alloc));
+            defs.append_all(item_struct.serdes_rust(no_alloc, true));
         }
     }
     defs
 }
 
-fn output_struct(defs: &mut TokenStream, method_ident: &Ident, return_type: &Type, no_alloc: bool) {
-    if matches!(return_type, Type::External(_, _)) {
-        return;
-    }
-    let ident = Ident::new(
-        format!("{}_output", method_ident)
-            .to_case(convert_case::Case::Pascal)
-            .as_str(),
-        method_ident.span(),
-    );
-    let mut item_struct = ItemStruct {
-        docs: Docs::empty(),
-        derive: vec![],
-        ident,
-        fields: vec![Field {
-            docs: Docs::empty(),
-            id: 0,
-            ident: Ident::new("output", Span::call_site()),
-            ty: return_type.clone(),
-            since: None,
-            default: None,
-        }],
-        cfg: None,
-        size_assumption: None,
-    };
-    create_flags(&mut item_struct.fields, &[]);
-    defs.append_all(item_struct.def_rust(no_alloc));
-    defs.append_all(item_struct.serdes_rust(no_alloc));
-}
+// fn output_struct(defs: &mut TokenStream, method_ident: &Ident, return_type: &Type, no_alloc: bool) {
+//     if matches!(return_type, Type::External(_, _)) {
+//         return;
+//     }
+//     let ident = Ident::new(
+//         format!("{}_output", method_ident)
+//             .to_case(convert_case::Case::Pascal)
+//             .as_str(),
+//         method_ident.span(),
+//     );
+//     let mut item_struct = ItemStruct {
+//         docs: Docs::empty(),
+//         derive: vec![],
+//         ident,
+//         fields: vec![Field {
+//             docs: Docs::empty(),
+//             id: 0,
+//             ident: Ident::new("output", Span::call_site()),
+//             ty: return_type.clone(),
+//             since: None,
+//             default: None,
+//         }],
+//         cfg: None,
+//         size_assumption: None,
+//     };
+//     create_flags(&mut item_struct.fields, &[]);
+//     defs.append_all(item_struct.def_rust(no_alloc));
+//     defs.append_all(item_struct.serdes_rust(no_alloc));
+// }

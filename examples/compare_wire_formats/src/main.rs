75432b22ac80a3fb5f61518f31847451f0086d7b
Roman Isaikin <romix.lab@gmail.com>
2026-02-22 16:32:22 +0100

    refactor: use Serde in compare wire formats example

--- examples/compare_wire_formats/src/main.rs
+++ examples/compare_wire_formats/src/main.rs
@@ -1,21 +1,21 @@
-use bincode::Encode;
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 use wire_weaver::prelude::*;
 
 #[derive_shrink_wrap]
+#[derive(Serialize)]
 struct Request {
     pub seq: u16,
     pub path_kind: PathKind,
     pub kind: RequestKind,
 
-    // Relocate is_some flags here to avoid loosing 7 bits on padding on each Option
+    // Relocate is_some flags here to avoid losing 7 bits on padding on each Option
     #[flag]
     pub dummy_c: bool,
     #[flag]
     pub dummy_b: bool,
     #[flag]
     pub dummy_a: bool,
-    // As an example use the remaining 5 bits of would-be-padding as well
+    // As an example, use the remaining 5 bits of would-be-padding as well
     pub flags: [bool; 5],
 
     pub dummy_a: Option<u8>,
@@ -26,6 +26,7 @@ struct Request {
 }
 
 #[derive_shrink_wrap]
+#[derive(Serialize)]
 #[ww_repr(u4)]
 enum PathKind {
     Absolute { path: Vec<UNib32> },
@@ -34,32 +35,9 @@ enum PathKind {
 }
 
 #[derive_shrink_wrap]
+#[derive(Serialize)]
 #[ww_repr(u4)]
 enum RequestKind {
-    Call { args: Vec<u8> },
-    Read,
-}
-
-#[derive(Serialize, Deserialize, Encode)]
-struct RequestSerde {
-    pub seq: u16,
-    pub path_kind: PathKindSerde,
-    pub kind: RequestKindSerde,
-    pub flags: [bool; 5],
-    pub dummy_a: Option<u8>,
-    pub dummy_b: Option<u8>,
-    pub dummy_c: Option<u8>,
-    pub strs: Vec<String>,
-}
-
-#[derive(Serialize, Deserialize, Encode)]
-enum PathKindSerde {
-    Absolute { path: Vec<u32> },
-    GlobalCompact,
-}
-
-#[derive(Serialize, Deserialize, Encode)]
-enum RequestKindSerde {
     Call { args: Vec<u8> },
     Read,
 }
@@ -83,27 +61,8 @@ fn main() {
     let bytes = req.to_ww_bytes(&mut scratch).unwrap();
     println!("WireWeaver: len: {}: {bytes:02X?}", bytes.len());
 
-    let req = RequestSerde {
-        seq: 1234,
-        path_kind: PathKindSerde::Absolute {
-            path: vec![0, 1, 2],
-        },
-        kind: RequestKindSerde::Call {
-            args: vec![0xAA, 0xBB, 0xCC],
-        },
-        flags: [false, true, false, true, false],
-        dummy_a: Some(0xCC),
-        dummy_b: None,
-        dummy_c: None,
-        strs: vec!["ab".to_string(), "c".to_string()],
-    };
-
     let bytes = postcard::to_slice(&req, &mut scratch).unwrap();
     println!("Postcard: len: {}: {bytes:02X?}", bytes.len());
-
-    let len = bincode::encode_into_slice(&req, &mut scratch, bincode::config::standard()).unwrap();
-    let bytes = &scratch[..len];
-    println!("Bincode: len: {}: {bytes:02X?}", bytes.len());
 
     let bytes = rmp_serde::to_vec(&req).unwrap();
     println!("MessagePack: len: {}: {bytes:02X?}", bytes.len());

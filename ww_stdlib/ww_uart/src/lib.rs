a7f0906ea93bb8886287a9149e7a8183be8e5390
Roman Isaikin <romix.lab@gmail.com>
2026-03-03 21:44:06 +0100

    refactor(ww_uart): simplify, use property Errors and derive ShrinkWrap

--- ww_stdlib/ww_uart/src/lib.rs
+++ ww_stdlib/ww_uart/src/lib.rs
@@ -8,7 +8,7 @@ pub trait Uart {
     /// Receive stream
     stream!(rx: RxChunk<'i>);
     /// Transmit sink
-    sink!(tx: TxChunk<'i>);
+    sink!(tx: [u8]);
     /// TX observe stream
     stream!(tx_mon: RxChunk<'i>);
 
@@ -16,43 +16,52 @@ pub trait Uart {
     fn capabilities() -> Capabilities<'i>;
 
     /// Baud rate configuration
-    property!(baud_rate: BaudRate/*, Error*/);
+    property!(baud_rate: BaudRate, Error);
 
     /// Mode configuration
-    property!(mode: Mode/*, Error*/);
+    property!(mode: Mode, Error);
 
     /// Stop bits configuration
-    property!(stop_bits: StopBits/*, Error*/);
+    property!(stop_bits: StopBits, Error);
 
     /// Parity configuration
-    property!(parity: Parity/*, Error*/);
+    property!(parity: Parity, Error);
 
     /// Detect RX low for more than 1 byte interval and set TX low to avoid powering device in sleep mode through it.
     /// Optional, check [Capabilities] if supported.
-    property!(prevent_back_feed: bool/*, Error*/);
+    property!(prevent_back_feed: bool, Error);
 
     /// Reference voltage configuration, if supported.
-    property!(reference_voltage: Volt/*, Error*/);
+    property!(reference_voltage: Volt, Error);
 
     /// Manually control pins, only in [Mode::Test]
     fn set_pin_level(pin: Pin, is_high: bool) -> Result<(), Error>;
 }
 
+#[derive_shrink_wrap]
+#[derive(Clone, Debug)]
+#[owned = "std"]
 pub struct RxChunk<'i> {
     pub flags: Option<RxFlags>,
     pub timestamp: Option<Second>,
     pub bytes: &'i [u8],
 }
 
+#[derive_shrink_wrap]
+#[derive(Clone, Debug)]
+#[owned = "std"]
 pub struct RxFlags {
     pub parity_error: bool,
 }
 
-pub struct TxChunk<'i> {
-    pub transmit_at: Option<Second>,
-    pub bytes: &'i [u8],
-}
+// pub struct TxChunk<'i> {
+//     pub transmit_at: Option<Second>,
+//     pub bytes: &'i [u8],
+// }
 
+#[derive_shrink_wrap]
+#[ww_repr(u4)]
+#[derive(Copy, Clone, Debug)]
 pub enum BaudRate {
     Baud110,
     Baud300,
@@ -69,6 +78,9 @@ pub enum BaudRate {
     BaudOther(u32),
 }
 
+#[derive_shrink_wrap]
+#[ww_repr(u4)]
+#[derive(Copy, Clone, Debug)]
 pub enum Mode {
     /// No flow control.
     Asynchronous,
@@ -87,6 +99,9 @@ pub enum Mode {
 }
 
 /// Pin selector used in test mode.
+#[derive_shrink_wrap]
+#[ww_repr(u2)]
+#[derive(Copy, Clone, Debug)]
 pub enum Pin {
     Tx,
     Rx,
@@ -95,19 +110,29 @@ pub enum Pin {
 }
 
 /// Number of stop bits transmitted after every character.
+#[derive_shrink_wrap]
+#[ww_repr(u1)]
+#[derive(Copy, Clone, Debug)]
 pub enum StopBits {
     Stop1,
-    Stop2
+    Stop2,
 }
 
 /// Parity checking mode.
+#[derive_shrink_wrap]
+#[ww_repr(u2)]
+#[derive(Copy, Clone, Debug)]
 pub enum Parity {
     None,
     Odd,
     Even,
 }
 
+#[derive_shrink_wrap]
+#[ww_repr(u4)]
+#[derive(Clone, Debug)]
 pub enum Error {
+    Unsupported,
     UnsupportedMode,
     UnsupportedReferenceVoltage,
     WrongMode,
@@ -116,15 +141,18 @@ pub enum Error {
     // CustomBytes(&'i [u8])
 }
 
+#[derive_shrink_wrap]
+#[derive(Clone, Debug)]
+#[owned = "std"]
 pub struct Capabilities<'i> {
     pub min_baud_rate: u32,
     pub max_baud_rate: u32,
     pub voltages: RefVec<'i, Volt>,
     pub rx_timestamps: bool,
-    pub transmit_at: bool,
     pub hw_flow_control: bool,
     pub sw_flow_control: bool,
     pub high_z_mode: bool,
     pub test_mode: bool,
     pub back_feed_detector: bool,
+    // pub transmit_at: bool,
 }
